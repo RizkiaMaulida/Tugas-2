cbcfb8f44d993e007dea3aec967039846ae721b0
RizkiaMaulida <Maulidarizkia096@gmail.com>
2026-09-19 20:30:52 +0800

    Add files via upload

---
+++ LuasSegitiga.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tipedata;
+
+/**
+ *
+ * @author Asus
+ */
+public class LuasSegitiga {
+    public static void main(String[] args) {
+        int alas = 5;
+       double tinggi = 3;
+     
+                System.out.println("luas segitiga:" + alas * tinggi/2);
+    }
+}
